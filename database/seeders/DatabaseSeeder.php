d23ba4e231328f26fa08edaa8066fa3e2fbdf82f
Thamoddya Rashmitha <thamoddyarashmithadissanayake@gmail.com>
2024-10-22 14:35:00 +0530

    Seeders Create

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Hash;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
+use Spatie\Permission\Models\Role;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +15,63 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $permissions = [
+            'create-receptionist',
+            'edit-receptionist',
+            'delete-receptionist',
+            'view-receptionist',
+            'create-student',
+            'edit-student',
+            'delete-student',
+            'view-student',
+            'create-invoice',
+            'edit-invoice',
+            'delete-invoice',
+            'view-invoice',
+            'create-payment',
+            'edit-payment',
+            'delete-payment',
+            'view-payment',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create([
+                'name' => $permission,
+            ]);
+        }
+
+        $superAdminRole = Role::create(
+            [
+                "name" => "Super_Admin",
+            ]
+        );
+
+        $receptionistRole = Role::create(
+            [
+                "name" => "Receptionist",
+            ]
+        );
+
+        $superAdminRole->givePermissionTo($permissions);
+
+        $receptionistRole->givePermissionTo([
+            'create-student',
+            'create-payment',
+            'edit-student',
+            'view-student',
+            'view-invoice',
+            'view-payment',
+        ]);
+
+        $superAdminOne = \App\Models\User::factory()->create([
+            'name' => 'Thamoddya Rashmitha',
+            'email' => 'thamo@gmail.com',
+            'nic' => '200000000V',
+            'password' => Hash::make('1234'),
+        ]);
+
+        $superAdminOne->assignRole($superAdminRole);
+
     }
 }
